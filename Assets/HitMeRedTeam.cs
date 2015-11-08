0b1eb7c7449c859a020efe7fec4bc3b4124be876
Andre Cardoso <amc.acardoso@gmail.com>
2015-11-08 17:35:13 -0500

    I don't even know a lot of changes

---
+++ Assets/HitMeRedTeam.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitMeRedTeam : MonoBehaviour {
+
+	public int bluescore;
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "RedBalloon") { //balloon can be changed to specifc color tag
+			bluescore = bluescore + 1; 
+			Debug.Log ("Blue Score = " + bluescore);
+			
+		}
+	}
+}
