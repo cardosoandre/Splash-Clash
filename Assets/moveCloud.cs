4fdc53ae6d210598c6e42557de313ee7d90634a3
Andre Cardoso <amc.acardoso@gmail.com>
2015-11-12 15:32:54 -0500

    Added Christina Sprites Made MoveCloud script

---
+++ Assets/moveCloud.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCloud : MonoBehaviour {
+
+	public float speed;
+	public float rightlimit = 10;
+	public float leftlimit = -10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += Vector3.right * speed * Time.deltaTime;
+		if (transform.position.x > rightlimit) {
+			transform.position = new Vector3 (leftlimit,transform.position.y, 2.58f);
+	
+	}
+}
+}
