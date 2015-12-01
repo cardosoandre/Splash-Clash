a31db69350355247e4312f30ee5f0580fdb3ae49
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MacBook-Pro.local>
2015-12-01 15:10:17 -0500

    no message

---
+++ Assets/Scripts/blueBalloonCounter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blueBalloonCounter : MonoBehaviour {
+
+	public balloonSupply;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		balloonSupply = 15;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if
+
+	}
+}
