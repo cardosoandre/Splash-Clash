6aff8d0ca9219f9566c95beeac21474f64134930
Christina Goefron <cagoefron@gmail.com>
2015-12-17 13:04:23 -0500

    Added Start Screen animation, sound effects

---
+++ Assets/Prefabs/StartScreenPrefabs/AniAudio1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AniAudio1 : MonoBehaviour {
+
+	public AudioClip aniClip1;
+	AudioSource audio;
+
+	void Start(){
+		audio = GetComponent<AudioSource> ();
+		audio.PlayOneShot (aniClip1);
+	}
+
+	void Update(){
+
+	}
+}
